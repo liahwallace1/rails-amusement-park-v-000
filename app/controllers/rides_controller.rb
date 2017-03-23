b2b3dc54fb84f57326d3de92e2aed7c444ea2c0a
Liah Wallace <liahwallace1@gmail.com>
2017-03-23 22:09:32 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,7 +3,7 @@ class RidesController < ApplicationController
     if logged_in?
       @ride = current_user.rides.create(attraction_id: params[:attraction_id])
       @message = @ride.take_ride
-      redirect_to user_path(@ride.user, :message => @message)
+      redirect_to user_path(@ride.user), :alert => @message
     else
       redirect_to login_path, flash[:notice] => "You need to log in first."
     end
